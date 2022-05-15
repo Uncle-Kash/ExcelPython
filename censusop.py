05453276c5b6955a73bb7a7f5da4c8a3e16fa8d2
Uncle Kash <unclekash@pm.me>
2022-05-15 18:54:13 +0100

    Open file and write census data to it

--- censusop.py
+++ censusop.py
@@ -7,3 +7,12 @@ countyData = {}
 
 # TODO: Fill in countyData with each county's population and tracts.
 
+print('Reading rows....')
+
+for row in range(2, sheet.max_row + 1):
+    state = sheet['B' + str(row)].value
+    county = sheet['C' + str(row)].value
+    pop = sheet['D' + str(row)].value
+
+with open('censusdata.txt', 'a') as file:
+    file.write(f"{state},{county},{pop}")

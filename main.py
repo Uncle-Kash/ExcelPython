e250bd7e4406c6512f91cb3648aa0dcc7fd831a2
Uncle Kash <unclekash@pm.me>
2022-05-15 16:15:32 +0100

    Create sheet object and store in variable'

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ print(type(wb))
 sheets = wb.sheetnames
 
 print(sheets)
+
+sheet1 = wb['Sheet1']
+
+myLocation = sheet1['A1']
+
+print(myLocation.value)
+print(myLocation.row)
+print(myLocation.column)
+print(myLocation.coordinates)

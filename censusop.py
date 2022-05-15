a247d4613b64700488aa971bf04fa5543b3ac811
Uncle Kash <unclekash@pm.me>
2022-05-15 17:42:34 +0100

    Import openpyxl and pprint modules

--- censusop.py
+++ censusop.py
@@ -1,4 +1,9 @@
-import openpyxl
+import openpyxl, pprint
 
-wb = openpyxl.load_workbook('/censuspopdata.xlsx')
-print(wb)
+wb = openpyxl.load_workbook('censuspopdata.xlsx')
+
+sheet = wb['Population by Census Tract']
+countyData = {}
+
+# TODO: Fill in countyData with each county's population and tracts.
+
